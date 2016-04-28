4c2a899196031392695a5dcbd8345d968c4bedf0
Alex Regan <alex.joseph.regan@gmail.com>
2016-04-27 18:25:19 -0700

    added support for chat rooms

--- public/js/app.js
+++ public/js/app.js
@@ -2,8 +2,14 @@ var name = getQueryVariable('name') || 'User';
 var room = getQueryVariable('room');
 var socket = io();
 
+$('room-title').text(room);
+
 socket.on('connect', function () {
 	console.log('Connected to socket.io server');
+	socket.emit('joinRoom', {
+		name: name,
+		room: room
+	});
 });
 
 socket.on('message', function (message) {
